0337ff0c62e8e3a0ff2ce0270eeaa4901dba5ca1
jonathancdays <jonathancdays@gmail.com>
2024-05-20 16:19:50 -0400

    Clean Up: Remove unecessary dirs and files

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -138,5 +138,5 @@ declare module 'astro:content' {
 
 	type AnyEntryMap = ContentEntryMap & DataEntryMap;
 
-	export type ContentConfig = typeof import("../src/content/config.js");
+	export type ContentConfig = never;
 }
