5079d8b100884eebf5c85c1c775721b1a71018bf
jonathancdays <jonathancdays@gmail.com>
2024-05-20 16:02:37 -0400

    Host: Remove other unecessary deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import htmx from "astro-htmx";
 
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [htmx(), mdx(), sitemap(), tailwind()],
+  integrations: [sitemap(), tailwind()],
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
 });
