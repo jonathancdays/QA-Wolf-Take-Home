783bd5d12d527e57fbb23e03a28a45300d319ff7
jonathancdays <jonathancdays@gmail.com>
2024-05-20 16:30:22 -0400

    Clean Up: Remove unecessary dirs and files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
